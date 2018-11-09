962b70fbb4c036f8790379119106e3fb90a5ba7a
chiessijlb <chiessijlb@yahoo.com.br>
2018-11-09 20:34:07 -0200

    comando delete()

--- 08-excluir registro.php
+++ 08-excluir registro.php
@@ -1,18 +1,6 @@
 <?php 
 require_once( "_config.php" );
 
-/*
-// Alterar um usuário
-$usuario = new Usuario();
-
-$usuario->loadById( 8 );
-
-$usuario->update( "professor", "!@#$%¨&*" );
-
-echo $usuario;
-*/
-
-/*
 // Excluir um usuário
 $usuario = new Usuario();
 
@@ -20,5 +8,4 @@ $usuario->loadById( 7 );
 $usuario->delete();
 
 echo $usuario;
-*/
 ?>
